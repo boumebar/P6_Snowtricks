1b0ded2c8ee9bf9f7521239c7e4170d628bb6ebe
boumediene <boume13@hotmail.com>
2021-11-09 00:49:38 +0100

    create index function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class HomeController extends AbstractController
 {
@@ -16,15 +15,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return new Response($this->render("pages/home.html.twig"));
-    }
-
-    /**
-     * @Route("/inde", name="inde")
-     * @return Response
-     */
-    public function inde(): Response
-    {
-        return new Response('je suis la inde');
+        return $this->render("pages/home.html.twig");
     }
 }
