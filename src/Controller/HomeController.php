5ba3c90233f1ce3fc955a88311e01def7f069a4e
boumediene <boume13@hotmail.com>
2021-11-09 12:26:40 +0100

    create Trick model, repository

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +12,22 @@ class HomeController extends AbstractController
 {
 
     /**
+     * @var TrickRepository
+     */
+    private $repository;
+
+    public function __construct(TrickRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
      * 
      * @return Response
      */
     public function index(): Response
     {
-        return $this->render("pages/home.html.twig");
+        $tricks = $this->repository->findAll();
+        return $this->render("pages/home.html.twig", ["tricks" => $tricks]);
     }
 }
