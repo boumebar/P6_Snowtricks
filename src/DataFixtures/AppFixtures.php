1e759ab697debfa28684f17a576ece1e3738a258
boumediene <boume13@hotmail.com>
2021-11-24 16:43:50 +0100

    add hash password

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,25 +5,35 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $admin = new User;
+        $hash = $this->encoder->hashPassword($admin, "password");
 
         $admin->setUsername("Admin")
             ->setEmail("admin@gmail.com")
-            ->setPassword("password")
+            ->setPassword($hash)
             ->setRoles(['ROLE_ADMIN']);
 
         $manager->persist($admin);
 
         for ($i = 0; $i < 5; $i++) {
             $user = new User;
+            $hash = $this->encoder->hashPassword($user, "password");
             $user->setUsername("user$i")
                 ->setEmail("user$i@gmail.com")
-                ->setPassword("password");
+                ->setPassword($hash);
             $manager->persist($user);
         }
 
